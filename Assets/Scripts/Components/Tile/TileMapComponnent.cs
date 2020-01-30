84d207b983de1b87e654f4a9c3da15d07c62f346
Victor Kaike <vkaike9@gmail.com>
2020-01-30 20:04:48 -0300

    commitando

---
+++ Assets/Scripts/Components/Tile/TileMapComponnent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Components.Tile
+{
+    public class TileMapComponnent : BaseComponent
+    {
+        #region PUBLIC ATTRIBUTES
+        public EnumSide Side => _side;
+        #endregion
+
+        #region SERIALIZE ATTRIBUTES
+        [Header("Required Methods")]
+        [SerializeField]
+        private EnumSide _side;
+        #endregion
+
+        #region PRIVATE ATTRIBUTES
+        private SpriteRenderer _spriteRenderer;
+        #endregion
+
+        #region PUBLIC METHODS
+        public void SetInitialSprite(Sprite sprite)
+        {
+            _spriteRenderer.sprite = sprite;
+        }
+        #endregion
+
+        #region ABSTRACT METHODS
+        protected override void SetInitialValues()
+        {
+            _spriteRenderer = this.GetComponent<SpriteRenderer>();
+        }
+
+        protected override void ValidateValues()
+        {
+        }
+        #endregion
+    }
+
+    public enum EnumSide
+    {
+        LEFT_UP,
+        UP,
+        RIGHT_UP,
+        RIGHT,
+        LEFT,
+        LEFT_BOTTOM,
+        BOTTOM,
+        BOTTOM_RIGHT,
+        CENTER
+    }
+}
---
+++ Assets/Scripts/Components/Tile/TileMapRandomGeneratorComponent.cs
@@ -0,0 +1,98 @@
+﻿using Assets.Scripts.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Components.Tile
+{
+    public class TileMapRandomGeneratorComponent : BaseComponent
+    {
+        #region SERIALIZABLE ATTRIBUTES
+        [Header("Configuration Fields")]
+        [SerializeField] 
+        private List<Sprite> LeftUpSpriteList;
+        [SerializeField] 
+        private List<Sprite> UpSpriteList;
+        [SerializeField] 
+        private List<Sprite> RightUpSpriteList;
+        [SerializeField] 
+        private List<Sprite> RightSpriteList;
+        [SerializeField] 
+        private List<Sprite> LeftSpriteList;
+        [SerializeField] 
+        private List<Sprite> BottomLeftSpriteList;
+        [SerializeField] 
+        private List<Sprite> BottomSpriteList;
+        [SerializeField] 
+        private List<Sprite> BottomRightSpriteList;
+        [SerializeField] 
+        private List<Sprite> CenterSpriteList;
+        #endregion
+
+        #region PRIVATE ATTRIBUTES
+        private TileMapComponnent[] _tileMapChildrensList;
+        #endregion
+
+        #region UNITY METHODS
+        private void Start()
+        {
+            this.StartEveryTile();
+        }
+        #endregion
+
+        #region PRIVATE METHODS
+        private void StartEveryTile()
+        {
+            foreach (TileMapComponnent tileMap in _tileMapChildrensList)
+            {
+                switch (tileMap.Side)
+                {
+                    case EnumSide.LEFT_UP:
+                        tileMap.SetInitialSprite(LeftUpSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.UP:
+                        tileMap.SetInitialSprite(UpSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.RIGHT_UP:
+                        tileMap.SetInitialSprite(RightUpSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.RIGHT:
+                        tileMap.SetInitialSprite(RightSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.LEFT:
+                        tileMap.SetInitialSprite(LeftSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.LEFT_BOTTOM:
+                        tileMap.SetInitialSprite(BottomLeftSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.BOTTOM:
+                        tileMap.SetInitialSprite(BottomSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.BOTTOM_RIGHT:
+                        tileMap.SetInitialSprite(BottomRightSpriteList.GetRandomElement());
+                        break;
+                    case EnumSide.CENTER:
+                        tileMap.SetInitialSprite(CenterSpriteList.GetRandomElement());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        #endregion
+
+        #region ABSTRACT METHODS
+        protected override void SetInitialValues()
+        {
+            _tileMapChildrensList = this.GetComponentsInChildren<TileMapComponnent>();
+        }
+
+        protected override void ValidateValues()
+        {
+        }
+        #endregion
+    }
+}
